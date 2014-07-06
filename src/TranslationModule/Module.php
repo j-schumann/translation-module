0bc344cf237ea4e4594bd934b5072ea0b0b91a68
Ellie Admin <admin@ellievantgrey.com>
2014-07-06 18:03:09 +0200

    upd: cleanup

---
+++ src/TranslationModule/Module.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @copyright   (c) 2014, Vrok
+ * @license     http://customlicense CustomLicense
+ * @author      Jakob Schumann <schumann@vrok.de>
+ */
+
+namespace TranslationModule;
+
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+/**
+ * Module bootstrapping.
+ */
+class Module implements AutoloaderProviderInterface, BootstrapListenerInterface,
+        ConfigProviderInterface
+{
+    /**
+     * Returns the autoloader definiton to use to load classes within this module.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Returns the modules default configuration.
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Link the translation service to the I18n\Translator.
+     *
+     * @param \Zend\EventManager\EventInterface $e
+     */
+    public function onBootstrap(EventInterface $e)
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $serviceManager = $e->getApplication()->getServiceManager();
+
+        $translationService = $serviceManager->get('TranslationModule\Service\Translation');
+        $translationService->attach($eventManager);
+    }
+}
