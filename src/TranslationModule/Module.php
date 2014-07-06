9bb68cad832db0e8cb09e051b412a392926eb35d
Ellie Admin <admin@ellievantgrey.com>
2014-07-06 18:10:03 +0200

    fix: config path

--- src/TranslationModule/Module.php
+++ src/TranslationModule/Module.php
@@ -41,7 +41,7 @@ class Module implements AutoloaderProviderInterface, BootstrapListenerInterface,
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     /**
